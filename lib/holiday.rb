38e2a832364b28df7c699bcc12d36d31d22ebe94
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-26 15:04:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,8 +62,10 @@ holiday_hash.each do |season, holiday|
   holiday.each do |holiday, supplies|
     if holiday == :winter 
       supplies << supply
-      supplies
+      
 end
+end supplies
+end 
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
