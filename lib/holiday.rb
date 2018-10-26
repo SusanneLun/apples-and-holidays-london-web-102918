1ae8edb15b456032161ecc32b9c920f9baf1600a
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-26 15:17:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,7 +90,13 @@ puts "#{season.to_s.capitalize}:"
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holidays_with_bbq = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, supplies|
+      holidays_with_bbq << holiday if supplies.include?("BBQ")
+    end
+  end
+  holidays_with_bbq
 end
 
 
