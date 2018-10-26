790eaf191bb23bc7e432dd0a8bcf39e333271b00
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-26 15:20:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,8 +85,6 @@ puts "#{season.to_s.capitalize}:"
  	    puts "  #{h_arr.join(" ")}: #{supplies.join(", ")}"
     end   
    end     
-end       
-end 
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
